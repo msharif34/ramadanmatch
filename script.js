96304d0181373f4c9eeb071f73e442491c8fdffc
msharif34 <msharif34@yahoo.com>
2015-07-09 18:57:49 -0700

    added js to the html and css

--- script.js
+++ script.js
@@ -1,59 +1,145 @@
 $(function(){
 	console.log('page ready');
+	//swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
 
-  var playerOneName = $('#playerOne');
-  var playerTwoName= $('#playerTwo');
-  var startButton = $('#start-button');
-  var formWrap = $('#form-wrap');
+	var error = ('<div class="alert alert-danger" role="alert">Please Enter Both Names.</div>');
 
-			  $('#playerOne').on('submit',function(e){
-			    e.preventDefault();
-			    console.log('player one submitted name');
+			  		 	$(".card").flip({
+						  axis: 'x',
+						  trigger: 'manual'
+						});
+			  			  	
+	$('#playerForm').on('submit',function(e){
+		e.preventDefault();
 
-			    var playerOneName = $('#player-info').val();
-			    console.log('Player one name: ', playerOneName)
-			    var playerTwoName = $('#player-info-two').val();
-
-			    console.log('Player two name: ', playerTwoName);
-
-			 var backgroundImg ='<div id="backgroundImg"<img src="http://previews.123rf.com/images/maxxxjo/maxxxjo1311/maxxxjo131100188/24146429-Seamless-pattern-stock-vector-used-for-tiling-background-Stock-Vector.jpg" style="height:100px;width:250px;" alt="25"/></div>';
-			 console.log('got image');
-
-			     if ($('#player-info').val() === '' || $('#player-info-two').val() === ''){
-
-			     	$('#viewport').append('<div class="alert alert-danger" role="alert">Please Enter Both Names.</div>');
-			     }
-
-			     else{
-			   
-			   		$('#player-one').append('<img src="http://arcadecomedytheater.com/wp-content/uploads/2013/04/PlayerOne_header.jpg" style="height:100px;width:250px;" alt="25"/>');
-					$('#player-two').append('<img src="http://s3.amazonaws.com/rapgenius/18458-85862-Player2jpg-620x.jpg" style="height:100px;width:250px;" alt="25"/>');
-					
-					
-					// $('#newport').fadeOut(3000);
-			
-					// $('#backgroundImg').fadeIn();
-
-					$('#newport').fadeOut("slow");
-       				// $('#backgroundImg').css("visibility: visible");
-       				$('#viewport').append('<div id="backgroundImg"><img src="http://previews.123rf.com/images/maxxxjo/maxxxjo1311/maxxxjo131100188/24146429-Seamless-pattern-stock-vector-used-for-tiling-background-Stock-Vector.jpg" style="height:100px;width:250px;" alt="25"/></div>');
-
-					
-				}
-
-				});
-
+		var playerOneName = $('#player-info').val();
+		console.log('Player one name: ', playerOneName)
+		var playerTwoName = $('#player-info-two').val();
 		
-	// ('<img src="http://arcadecomedytheater.com/wp-content/uploads/2013/04/PlayerOne_header.jpg">');
+
+		console.log('Player two name: ', playerTwoName);
+
+
+		if ($('#player-info').val() === '' || $('#player-info-two').val() === ''){
+			swal({   
+				title: "Error!",   
+				text: "Please enter both player names!",   
+				type: "error",   
+				confirmButtonText: "Try Again" 
+
+			});
+
+			} else {
+			swal.close();
+			$('.player-one-score .name').html(playerOneName);
+			$('.player-two-score .name').html(playerTwoName);
+			$('#playerForm').fadeOut();
+
+		}
+	});
+
+	var counter = 0;
+	var score = [0,0];
+	var turn = 0;
+
+
+	$('.card').on('click', function(){
+		// if ($('.selected').find('.back img').attr('src') === $(this).find('.back img').attr('src') ){
+		// 	console.log('its a match');
+		// }
+		var previous = $('.selected');
+		var current = $(this);
+		
+		if($(this).hasClass('selected')){
+			$(this).removeClass('selected');
+			$(this).flip(false);
+		}else{
+			$(this).addClass('selected');
+			$(this).flip(true);
+
+			if(previous.length > 0){
+				setTimeout(function(){
+					if(previous.find('.back img').attr('src') == current.find('.back img').attr('src')){
+						//correct match
+						$('.selected').removeClass('selected').fadeOut();
+						counter +=2;
+						score[turn]+=2;
+						console.log(counter);
+
+						$('.player-one-score .score').html(score[0])
+						$('.player-two-score .score').html(score[1])
+						
+						
+
+
+						if (counter === 6){
+							
+							//reset cards
+							$('.card').flip(false);
+							$('.card').fadeIn('slow');
+							counter = 0;
+							//reset score
+							$('.player-one-score .score').html(0);
+							$('.player-two-score .score').html(0);
+							
+									if (score[0] > score[1]){
+										swal({   
+											title: "Game Over!",   
+											text: "Player ONE wins!",   
+											type: "success",   
+											confirmButtonText: "Play Again?" 
+
+										});
+									}else{
+										swal({   
+											title: "Game Over!",   
+											text: "Player TWO wins!",   
+											type: "success",   
+											confirmButtonText: "Play Again?" 
+
+										});
+
+									}
+
+						}
+
+					
+
+
+					}else{
+						//wrong match
+						$('.selected').flip(false);
+						$('.selected').removeClass('selected')
+						turn = Math.abs(turn - 1);
+						if (turn === 0){
+							swal({   
+									title: "Wrong Guess!",   
+									text: "Player one's turn!",   
+									type: "error",   
+									confirmButtonText: "Try Again" 
+
+								});
+						}else{
+							swal({   
+									title: "Wrong Guess!",   
+									text: "Player two's turn!",   
+									type: "error",   
+									confirmButtonText: "Try Again" 
+
+								});
+						}
+					
+
+					}
+
+
+				},500);
+			}
+		}
+		
+
+	});
 
 
 
-
-
-
-
-
-
-
-
-})
+});
